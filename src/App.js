76b7def0ddb3b9d46bd53e4ef6ff94db24b652d8
NgocChau0403 <21521885@gm.uit.edu.vn>
2024-11-02 13:55:43 +0700

    Login + Signup + Forgot password

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ import Contact from "./pages/Contact";
 import OurStore from "./pages/OurStore";
 import Blogs from "./pages/Blogs";
 import CompareProduct from "./pages/CompareProduct";
-import Wishlist from ".pages/Wishlist";
+import Wishlist from "./pages/Wishlist";
+import Login from "./pages/Login";
+import Forgotpassword from "./pages/Forgotpassword";
+import Signup from "./pages/Signup";
+import Resetpassword from "./pages/Resetpassword";
 
 function App() {
   return (
@@ -23,6 +27,10 @@ function App() {
             <Route path="blogs" element={<Blogs />} />
             <Route path="compare-product" element={<CompareProduct />} />
             <Route path="wishlist" element={<Wishlist />} />
+            <Route path="login" element={<Login />} />
+            <Route path="forgot-password" element={<Forgotpassword />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="reset-password" element={<Resetpassword />} />
           </Route>
         </Routes>
       </BrowserRouter>
